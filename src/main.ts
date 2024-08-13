40e39843aaa31a1dd92230916f4e68994fd71a0d
Jeong Quokka <107650362+chipmunk-dev@users.noreply.github.com>
2024-08-13 23:48:10 +0900

    #1 Swagger 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Hamkke API Document')
+    .setDescription('함께 서비스 API 문서입니다.')
+    .setVersion('1.0')
+    .addTag('User')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(8080);
 }
 bootstrap();
