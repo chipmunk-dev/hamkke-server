8d63731c137ee23a7ccd9a4f4f6c5c42beda2cec
Jeong Quokka <107650362+chipmunk-dev@users.noreply.github.com>
2024-08-14 01:26:26 +0900

    #1 typeorm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import ConfigModule from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DB_URL,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: false,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import swaggerConfig from './config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Hamkke API Document')
-    .setDescription('함께 서비스 API 문서입니다.')
-    .setVersion('1.0')
-    .addTag('User')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8080);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
