0375b1861752d6a00dcb516634e022f20c919ce2
Jeong Quokka <107650362+chipmunk-dev@users.noreply.github.com>
2024-08-14 01:39:21 +0900

    Feature/#1 set module (#2)
    * #1 Swagger 세팅
    
    * #1-docker 설정
    
    - APP Dockerfile 작성
    - Postgres 세팅
    
    * #1 typeorm 설정
    
    * #1 logging middleware
    
    - API 로그를 보는 미들웨어 작성
    
    * #1 apply eof
    
    * #1 공백 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import ConfigModule from './config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DB_URL,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: false,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
---
+++ src/config/configuration.ts
@@ -0,0 +1,7 @@
+export default () => ({
+  PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+});
