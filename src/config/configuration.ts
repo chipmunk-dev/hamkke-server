83fc7763034d63b28692878264074763d944792c
Jeong Quokka <107650362+chipmunk-dev@users.noreply.github.com>
2024-08-17 00:59:17 +0900

    #3 미사용 변수 제거

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,6 @@
 export default () => ({
   PORT: process.env.PORT,
-  DB_URL: process.env.DB_URL,
+  DB_HOST: process.env.DB_HOST,
   DB_NAME: process.env.DB_NAME,
   DB_USER: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
