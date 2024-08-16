d3d6f1da4f546ece91411a230baf2edc7f7f930a
Jeong Quokka <107650362+chipmunk-dev@users.noreply.github.com>
2024-08-17 00:58:44 +0900

    #3 유효성 검사 수정

--- src/users/dto/login-user.dto.ts
+++ src/users/dto/login-user.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class LoginUserDto {
   @IsEmail()
-  @IsNotEmpty()
   @ApiProperty({
     type: String,
     description: '사용자의 이메일',
@@ -14,6 +19,11 @@ export class LoginUserDto {
 
   @IsNotEmpty()
   @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      '비밀번호는 대문자, 소문자, 숫자 또는 특수 문자를 포함해야 합니다.',
+  })
   @ApiProperty({
     type: String,
     description: '사용자의 비밀번호',
