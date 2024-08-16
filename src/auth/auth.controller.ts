635ad2e1831fcc1b370c243532594180f8217acb
Jeong Quokka <107650362+chipmunk-dev@users.noreply.github.com>
2024-08-17 00:59:47 +0900

    #3 Swagger 업데이트

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,8 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from 'src/users/dto/login-user.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthResponseDto } from './dto/login-response.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -10,8 +11,22 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @ApiOperation({
-    summary: '이메일 기반 회원가입',
-    description: '사용자에게 정보를 입력받아 회원가입',
+    summary: '회원가입',
+    description: '새로운 사용자를 생성 및 토큰 발급',
+  })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({
+    status: 201,
+    description: '회원가입에 성공',
+    type: AuthResponseDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: '유효성 검사 실패',
+  })
+  @ApiResponse({
+    status: 409,
+    description: '이미 존재하는 이메일',
   })
   @Post('signup/email')
   async signup(@Body() data: CreateUserDto) {
@@ -19,8 +34,22 @@ export class AuthController {
   }
 
   @ApiOperation({
-    summary: '이메일 기반 로그인',
-    description: '사용자에게 정보를 입력받아 로그인',
+    summary: '로그인',
+    description: '사용자 인증 및 토큰 발급',
+  })
+  @ApiBody({ type: LoginUserDto })
+  @ApiResponse({
+    status: 200,
+    description: '로그인 성공',
+    type: AuthResponseDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: '존재하지 않는 이메일',
+  })
+  @ApiResponse({
+    status: 401,
+    description: '비밀번호 불일치',
   })
   @Post('login/email')
   async login(@Body() data: LoginUserDto) {
